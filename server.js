c2fb922e710fdc19b5bb9a0b7459f0df0c39c015
Jay <jbarbanel531@gmail.com>
2021-12-27 13:48:37 -0500

    added API post requests

--- server.js
+++ server.js
@@ -16,3 +16,31 @@ app.use(express.json());
 
 // Add a static middleware for serving assets in the public folder
 app.use(express.static("./Develop/public"));
+
+// Async processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// API Route "GET" request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data));
+        res.json(notes);
+    });
+});
+
+//API post requests
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./Develop/db/db.json", "utf8")
+        .then(function(data) {
+            const notes = [].concat(JSON.parse(data));
+            note.id = notes.length + 1;
+            notes.push(note);
+            return notes;
+        })
+        .then(function(notes) {
+            writeFileAsync("./Develop/db/db.json", JSON.stringify(notes));
+            res.json(note);
+        });
+});
